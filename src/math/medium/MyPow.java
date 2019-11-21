1b0ba06ef0109b110dd6c2d77f67f649b7791e6b
alery315 <sample29901@gmail.com>
2019-11-21 22:46:14 +0800

    2019.11.21 nums:97

---
+++ src/math/medium/MyPow.java
@@ -0,0 +1,61 @@
+package math.medium;
+
+/**
+ * @auther alery
+ * @create 2019-11-21 22:09
+ */
+public class MyPow {
+
+    public double myPow(double x, int n) {
+        double ans = 1;
+        if (n > 0) {
+            for (int i = 0; i < n; i++) {
+                ans *= x;
+            }
+        } else {
+            for (int i = 0; i < -n; i++) {
+                ans /= x;
+            }
+        }
+        return ans;
+    }
+
+    // 其实好像也不需要存储,这个想法太强了,快速幂思想
+    // 我觉得这里如果用double数组存储计算过的幂,会不会更快呢
+    public double myPow2(double x, int n) {
+        if (n == 0) { return 1; }
+        if (n == 1) { return x; }
+        if (n == -1) { return 1 / x; }
+        double half = myPow2(x, n / 2);
+        double rest = myPow2(x, n % 2);
+        return rest * half * half;
+    }
+
+    // 快速幂迭代写法
+    public long binaryPow(long a, long b) {
+        long ans = 1;
+        while (b > 0) {
+            if ((b & 1) == 1) {
+                ans = ans * a;
+            }
+            a = a * a;
+            b >>= 1;
+        }
+        return ans;
+    }
+
+
+    public static void main(String[] args) {
+
+        long  startTime = System.currentTimeMillis();    //获取开始时间
+
+        System.out.println(new MyPow().myPow2(2, 5));
+
+        System.out.println(new MyPow().binaryPow(2, 10));
+
+        long endTime = System.currentTimeMillis();    //获取结束时间
+
+        System.out.println("程序运行时间：" + (endTime - startTime) + "ms");    //输出程序运行时间
+
+    }
+}
--- src/math/medium/MySqrt.java
+++ src/math/medium/MySqrt.java
@@ -7,16 +7,49 @@ package math.medium;
 public class MySqrt {
 
     public int mySqrt(int x) {
-
-
         return (int)Math.sqrt(x);
     }
 
 
+
+    public int mySqrt2(int x) {
+        long l = 0, r = 99999;
+
+        while (l < r) {
+            // 这个写法很重要,可以选择右中位数(相对大的那个)
+            long mid = (l + r + 1) >>> 1;
+            long temp = mid * mid;
+            System.out.println(l + " : " + r);
+            if (temp < x) {
+                l = mid;
+            } else if (temp > x) {
+                r = mid - 1;
+            } else {
+                return (int) mid;
+            }
+        }
+        return (int)r;
+    }
+
+    public int sqrts(double x, double s){
+        if (x == 0) return 0;
+        double e = 1e-3;
+        double res = (x + s / x) / 2;
+        System.out.println(res * res);
+        if (res * res - s < e) {
+            return (int) res;
+        } else {
+            return sqrts(res, s);
+        }
+
+    }
+
+
+
     public static void main(String[] args) {
 
-        System.out.println(new MySqrt().mySqrt(8));
-
+//        System.out.println(new MySqrt(1).mySqrt2(2147395599));
+        System.out.println(new MySqrt().sqrts(183692038,183692038));
     }
 
 }
